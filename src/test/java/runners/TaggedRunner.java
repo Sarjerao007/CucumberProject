4835885407e040ff2467517eab50c228f3364801
Himmat <sachinbochare94@gmail.com>
2023-08-25 08:01:53 +0000

    Latest commit 25AUG

--- src/test/java/runners/TaggedRunner.java
+++ src/test/java/runners/TaggedRunner.java
@@ -10,12 +10,15 @@ import io.cucumber.testng.CucumberOptions;
 		monochrome=true,
 //		tags="@SmokeTest"
 //		tags="@RegressionTest"
+//		tags="not @SmokeTest"
 //		tags="@SmokeTest or @RegressionTest"
 //		tags="@SmokeTest and @RegressionTest"
 //		tags="@PhaseTwo"
 //		tags="@PhaseOne"
 //		tags="@PhaseOne and @SmokeTest"
-		tags="@PhaseOne and not @SmokeTest"
+//		tags="@PhaseOne and not @SmokeTest"
+//		tags="@PhaseOne and @SmokeTest or @PhaseTwo and @RegressionTest"
+		tags="@PhaseOne and not @SmokeTest and not @RegressionTest"
 		)
 public class TaggedRunner extends AbstractTestNGCucumberTests {
   
